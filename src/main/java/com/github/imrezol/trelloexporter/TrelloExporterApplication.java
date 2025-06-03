4584e7e07777fca1be463edcb51edb86751c3669
ZotyoNegentropics <zoltan.imre@negentropics.com>
2025-06-03 11:10:43 +0200

    Minimal spring shell

---
+++ src/main/java/com/github/imrezol/trelloexporter/TrelloExporterApplication.java
@@ -0,0 +1,14 @@
+package com.github.imrezol.trelloexporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloExporterApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrelloExporterApplication.class, args);
+	}
+
+}
